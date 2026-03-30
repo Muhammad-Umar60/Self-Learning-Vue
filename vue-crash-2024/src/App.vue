ccfbd0c23b9b0dac797e9c2dd0a70d1a753ae9cd
Muhammad-Umar60 <munaeem@nisum.com>
2026-03-30 20:08:49 +0500

    add components, views, routes.

--- vue-crash-2024/src/App.vue
+++ vue-crash-2024/src/App.vue
@@ -1,3 +1,11 @@
+<script setup>
+
+import Navbar from './components/Navbar.vue';
+import { RouterView } from 'vue-router';
+
+</script>
+
 <template>
-  <h1 class="text-5xl">Vue Jobs</h1>
+    <Navbar/>
+    <RouterView/>
 </template>
---
+++ vue-crash-2024/src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Hero from '../components/Hero.vue';
+import HomeCard from '../components/HomeCard.vue';
+import JobListings from '../components/JobListings.vue';
+
+</script>
+
+<template>
+    <Hero title="Test Title" subtitle="it is a subtitle"/>
+    <HomeCard/>
+    <JobListings :limit="3" :show-button="true"/>
+
+</template>
